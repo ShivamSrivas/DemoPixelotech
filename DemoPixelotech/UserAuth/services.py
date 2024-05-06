4fe8ad03bbae9b7e4c4820826a910cc5be63f8e1
Shivam Srivastava <shivamsri896@gmail.com>
2024-05-07 02:50:33 +0530

    UserRegisteration

--- DemoPixelotech/UserAuth/services.py
+++ DemoPixelotech/UserAuth/services.py
@@ -1,10 +1,26 @@
 import requests
+import random
+from Log.logger import logger_call
+
 
 class UserRegisteration():
+    def __init__(self,ProcessId):
+        self.ProcessId = ProcessId
+
     def UserRegistrationByPhone(self,PhoneNumber):
         try:
-            url=""
+            logger_call("Log/UserAuth.log",self.ProcessId,"UserRegistrationByPhone Is Invoked","Info")
+            otp = random.randint(1000,2222)
+            api_key="833244f2-0bde-11ef-8cbb-0200cd936042"
+            url=f"https://2factor.in/API/V1/{api_key}/SMS/{PhoneNumber}/{otp}"
             data=requests.get(url)
-            return data
+            if data.status_code:
+                logger_call("Log/UserAuth.log",self.ProcessId,"Otp is sent","Info")
+                return otp
         except Exception as e:
+            logger_call("Log/UserAuth.log",self.ProcessId,"There's an error in UserRegistrationByPhone method saying --> " + str(e),"Error")
             return e
+        
+
+obj = UserRegisteration(1234)
+print(obj.UserRegistrationByPhone(8853157886))
