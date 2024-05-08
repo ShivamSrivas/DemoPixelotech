cdcf6caa42564f49895539cb061e180c995ea6fa
Shivam Srivastava <shivamsri896@gmail.com>
2024-05-09 00:06:53 +0530

    RevertChanges

--- DemoPixelotech/DemoPixelotech/wsgi.py
+++ DemoPixelotech/DemoPixelotech/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DemoPixelotech.DemoPixelotech.settings")
-os.environ.setdefault("urls","DemoPixelotech.DemoPixelotech.urls")
+os.environ.setdefault("DJANGO_URLS_MODULE","DemoPixelotech.DemoPixelotech.urls")
 
 application = get_wsgi_application()
